8bcbb9f21d219cbcb7bd4b06b1c552b149126e1b
Aleksander Sirge <sirgealeksander@gmail.com>
2021-12-07 18:31:22 +0200

    add new projects

---
+++ FunctionsReturnAndArraysFrom/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FunctionsReturnAndArraysFrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandomFilm();
+        }
+
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, foods.Length);
+
+            string randomFood = foods[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFood}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, drinks.Length);
+
+            string randomDrink = drinks[randomIndex];
+            Console.WriteLine($"Computer picked: {randomDrink}");
+        }
+
+        private static void DisplayRandomFilm()
+        {
+            string[] films = { "horror", "comedy", "sci-fi", "drama" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, films.Length);
+
+            string randomFilm = films[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFilm}");
+        }
+    }
+}
